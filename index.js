d30d9757c156fdefe327ab8b6088988e563fd137
zhaowei <840728393@qq.com>
2019-07-15 15:54:34 +0800

    add

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-const config= require('./config/config')
-const Wechat = require('./wechat/wechat') 
+// const config= require('./config/config')
+// const Wechat = require('./wechat/wechat') 
 
-let wechat =new Wechat(config)
+var express = require('express')
+var app = express()
+app.set('views', './view')
+app.set('view engine', 'pug')
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Hey', message: 'Hello there!' })
+})
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+// let wechat =new Wechat(config)
